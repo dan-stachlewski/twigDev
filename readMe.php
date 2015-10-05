daf68c2c8d59d46918bbbca8b9ad91b87a847b9d
dan-stachlewski <dan.stachlewski@gmail.com>
2015-10-06 10:59:48 +1100

    Set Up new Project twigDev. Added COMPOSER Dependency TWIG V1.22.2 Set up Versioning in GitHub, created new Repository.

---
+++ readMe.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * STEP [1] Create NEW PROJECT
+ * STEP [2] RIGHT CLICK NEW PROJECT
+ * STEP [3] Choose COMPOSER -> ADD DEPENDENCY
+ * STEP [4] Search for TWIG
+ * STEP [5] Use VERSION 1.22.2 or "v^1.0"
+ * STEP [6] Click REQUIRE 
+ */
+
