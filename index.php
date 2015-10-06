5fe810685bd138ad3dd7b0924451f56a0945bad1
dan-stachlewski <dan.stachlewski@gmail.com>
2015-10-06 11:14:36 +1100

    Set Up a readMe.php file. Added CACHE & TEMPLATES DIRS Set up /index.php file w: - library autoloader - twig object

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+//Autoloads all the libraries maintained by composer
+require './vendor/autoload.php';
+
+/***
+ * If not using composer load libraries w below code:
+require_once '/path/to/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+*/
+
+//set up the twig object
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader, [
+    'cache' => './cache',
+    //will recompile everytime we run the file
+    'debug' => true,
+]);
--- readMe.php
+++ readMe.php
@@ -6,6 +6,19 @@
  * STEP [3] Choose COMPOSER -> ADD DEPENDENCY
  * STEP [4] Search for TWIG
  * STEP [5] Use VERSION 1.22.2 or "v^1.0"
- * STEP [6] Click REQUIRE 
+ * STEP [6] Click REQUIRE
+ * STEP [7] Set Up Versioning w GitHub and Committed files
+ * STEP [8] Set up CACHE DIR - store all files when DEBUGGING
+ * -TWIG uses environment to store config files
+ * STEP [9] Set up TEMPLATES DIR
+ * -TWIG uses loader to locate templates
+ * STEP [10] setup '/index.php' = index file in the app root DIR
+ * STEP [11] ALWAYS START index.php file w:
+ * - require './vendor/autoload.php';
+ * - as it Autoloads all the libraries maintained by composer
+ * STEP [12] Set up the Twig Object
+ * STEP []
+ * STEP []
+ *
  */
 
